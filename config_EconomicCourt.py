8bf4e4be0f8aa9f5f2f1bde997594729e3d9f9ca
rYauheni <ra.yauheni@gmail.com>
2022-10-07 14:29:35 +0300

    edit gitignore

--- config_EconomicCourt.py
+++ config_EconomicCourt.py
@@ -28,20 +28,25 @@ def choose_instance(message):
     bot.send_message(message.chat.id, 'Выберите судебную инстанцию', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: True)
+def bla(arg):
+    return True
+
+
+@bot.callback_query_handler(func=bla)
 def choose_instance_callback(call):
     if call.data == 'first_instance':
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Первая инстанция')  # удаляется предыдущее сообщение, чтобы было красиво
-#         bot.register_next_step_handler(call, choose_type_of_legal_proceeding_first_instance)
-#
-#
-# # @bot.message_handler(commands=['proceeding'])
-# def choose_type_of_legal_proceeding_first_instance(message):
-#     markup = types.InlineKeyboardMarkup(row_width=1)
-#     item1 = types.InlineKeyboardButton('Исковое производство', callback_data='isk')
-#     item2 = types.InlineKeyboardButton('Приказное производство', callback_data='prikaz')
-#     markup.add(item1, item2)
-#     bot.send_message(message.chat.id, 'Выберите вид судопроизводства', reply_markup=markup)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                              text='Первая инстанция')  # удаляется предыдущее сообщение, чтобы было красиво
+        bot.register_next_step_handler(call, choose_type_of_legal_proceeding_first_instance)
+
+
+# @bot.message_handler(commands=['proceeding'])
+def choose_type_of_legal_proceeding_first_instance(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton('Исковое производство', callback_data='isk')
+    item2 = types.InlineKeyboardButton('Приказное производство', callback_data='prikaz')
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, 'Выберите вид судопроизводства', reply_markup=markup)
 
 
 bot.infinity_polling()

4f854ac042cda6313f8e893083aeb24b5340976d
rYauheni <ra.yauheni@gmail.com>
2022-10-10 17:46:55 +0300

    attempt 1 after change the library

--- config_EconomicCourt.py
+++ config_EconomicCourt.py
@@ -1,52 +1,79 @@
-import telebot
-from telebot import types
+import logging
 
-token = '5675091266:AAHbP-X6DxIrQ5FqXwkn9Nt03ayzA74CP1Y'
-bot = telebot.TeleBot(token, parse_mode='HTML')
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Настоящий <i><b>бот</b></i> представляет собой калькулятор государственной '
-                                      'пошлины при обращениии в суды.\n\n'
-                                      'Расчёт государственной пошлины является примерным.\n\n'
-                                      'Сведения, полученные в результате работы настоящего бота, '
-                                      'не могут быть использованы в качестве доказательства в суде.')
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
-@bot.message_handler(commands=['instance'])
-def choose_instance(message):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item1 = types.InlineKeyboardButton('Производство в суде первой инстанции', callback_data='first_instance')
-    item2 = types.InlineKeyboardButton('Производство в суде апелляционной инстанции', callback_data='appeal')
-    item3 = types.InlineKeyboardButton('Производство в суде кассационной инстанции', callback_data='cassation')
-    item4 = types.InlineKeyboardButton('Производство по пересмотру судебных постановлений в порядке надзора',
-                                       callback_data='supervisory')
-    item5 = types.InlineKeyboardButton('Производство по вновь открывшимся обстоятельствам', callback_data='newly_facts')
-    item6 = types.InlineKeyboardButton('Иные процессуальные действия', callback_data='other')
-    markup.add(item1, item2, item3, item4, item5, item6)
-    bot.send_message(message.chat.id, 'Выберите судебную инстанцию', reply_markup=markup)
+def start_command(update: Update, context: CallbackContext) -> None:
+    """Sends a message with options for choosing a court using inline buttons"""
+    keyboard = [
+        [InlineKeyboardButton('Производство в суде первой инстанции', callback_data='first_instance')],
+        [InlineKeyboardButton('Производство в суде апелляционной инстанции', callback_data='appeal')],
+        [InlineKeyboardButton('Производство в суде кассационной инстанции', callback_data='cassation')],
+        [InlineKeyboardButton('Производство по пересмотру судебных постановлений в порядке надзора',
+                              callback_data='supervisory')],
+        [InlineKeyboardButton('Производство по вновь открывшимся обстоятельствам', callback_data='newly_facts')],
+        [InlineKeyboardButton('Иные процессуальные действия', callback_data='other')]
+    ]
+    logger.info("User %s started to choose some instance", update.message.from_user.first_name)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Выберите <i><b>судебную</b></i> инстанцию:', reply_markup=reply_markup,
+                              parse_mode='HTML')
 
 
-def bla(arg):
-    return True
+def button_choose_instance(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery when a court instance is selected, updates the message text,
+    and calls the select function of choosing a type of legal proceeding."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer('ASDFGHJKL')  # ВСПЛЫВАЮЩАЯ ПОДСКАЗКА
+
+    if query.data == 'first_instance':
+        query.edit_message_text(text='Вы выбрали:\n"Производство в суде первой инстанции"')
+    choose_type_of_legal_proceeding(update, context)
 
 
-@bot.callback_query_handler(func=bla)
-def choose_instance_callback(call):
-    if call.data == 'first_instance':
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
-                              text='Первая инстанция')  # удаляется предыдущее сообщение, чтобы было красиво
-        bot.register_next_step_handler(call, choose_type_of_legal_proceeding_first_instance)
+def choose_type_of_legal_proceeding(update: Update, context: CallbackContext) -> None:
+    """Sends a message with options for choosing a type of legal proceeding."""
+    keyboard = [
+        [InlineKeyboardButton('Исковое производство', callback_data='isk')],
+        [InlineKeyboardButton('Приказное производство', callback_data='prikaz')],
+    ]
+    logger.info("User %s started to choose some type of legal proceeding", update.message.from_user.first_name)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Выберите вид судопроизводства:', reply_markup=reply_markup)
 
 
-# @bot.message_handler(commands=['proceeding'])
-def choose_type_of_legal_proceeding_first_instance(message):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item1 = types.InlineKeyboardButton('Исковое производство', callback_data='isk')
-    item2 = types.InlineKeyboardButton('Приказное производство', callback_data='prikaz')
-    markup.add(item1, item2)
-    bot.send_message(message.chat.id, 'Выберите вид судопроизводства', reply_markup=markup)
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Displays info on how to use the bot."""
+    update.message.reply_text("Наберите (или выберите из меню) /start для начала работы.")
 
 
-bot.infinity_polling()
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater('5675091266:AAHbP-X6DxIrQ5FqXwkn9Nt03ayzA74CP1Y')
+
+    updater.dispatcher.add_handler(CommandHandler('start', start_command))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button_choose_instance))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()

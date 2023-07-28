4632b9b33bc4c36b7d84c6eb586c0c95de818462
rYauheni <ra.yauheni@gmail.com>
2023-07-28 14:03:24 +0300

    Changed settings.py: 1) base value constant change to 37.0 2) added constant ACTUALITY_CHECK_DATE

--- settings/settings.py
+++ settings/settings.py
@@ -1,6 +1,9 @@
-# Determines the size of the base value used in the calculations
-BASE_VALUE = 32.0
+# Determines the size of the base value used in the calculations.
+BASE_VALUE = 37.0
 
 # If True opens access to the function of calculating the state duty.
 # Needed in case of technical work or loss of relevance of the data on the basis of which the calculation is made
 ACCESS = True
+
+# Indicates the date as of which legal acts are applied.
+ACTUALITY_CHECK_DATE = '26.07.2023'

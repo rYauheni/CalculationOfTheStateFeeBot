205df0954981123fe0afc659fe989ae5773437bb
rYauheni <ra.yauheni@gmail.com>
2024-01-01 20:13:00 +0300

    Turn off access of app in settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -3,7 +3,7 @@ BASE_VALUE = 37.0
 
 # If True opens access to the function of calculating the state duty.
 # Needed in case of technical work or loss of relevance of the data on the basis of which the calculation is made
-ACCESS = True
+ACCESS = False
 
 # Indicates the date as of which legal acts are applied.
 ACTUALITY_CHECK_DATE = '26.07.2023'

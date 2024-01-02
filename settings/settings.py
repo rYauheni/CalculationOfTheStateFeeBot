a9ec9e2ddb0c944d9c60542db6178b33850de1ac
rYauheni <ra.yauheni@gmail.com>
2024-01-02 12:05:57 +0300

    Turn off access of app in settings.py Changed BASE_VALUE from 37.0 to 40.0 Changed ACTUALITY_CHECK_DATE to '02.01.2024'

--- settings/settings.py
+++ settings/settings.py
@@ -1,12 +1,12 @@
 # Determines the size of the base value used in the calculations.
-BASE_VALUE = 37.0
+BASE_VALUE = 40.0
 
 # If True opens access to the function of calculating the state duty.
 # Needed in case of technical work or loss of relevance of the data on the basis of which the calculation is made
-ACCESS = False
+ACCESS = True
 
 # Indicates the date as of which legal acts are applied.
-ACTUALITY_CHECK_DATE = '26.07.2023'
+ACTUALITY_CHECK_DATE = '02.01.2024'
 
 # Defines settings for development or production will be applied.
 PRODUCTION = True
